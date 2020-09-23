68b20c7675230fb9d48fcf0640cb282b1b3b304e
amatyaichhya <amatyaichhya@gmail.com>
2020-09-23 16:45:39 +0545

    authentication without api

--- authentication/views.py
+++ authentication/views.py
@@ -18,10 +18,10 @@ def register(request):
         phone = request.POST['Phone']
 
         response = requests.get(
-             #'https://jsonplaceholder.typicode.com/todos/1')
-            f'https://app.verify-email.org/api/v1/UBTW2fIw7W2yR5ICPldZGuah72wKN8gTsAB7aho06HoiKw9ySC/verify/{email}')
+             'https://jsonplaceholder.typicode.com/todos/1')
+            #f'https://app.verify-email.org/api/v1/UBTW2fIw7W2yR5ICPldZGuah72wKN8gTsAB7aho06HoiKw9ySC/verify/{email}')
         data = response.json();
-        if (data['status'] == 1):  # id=1
+        if (data['id'] == 1):  # status=1
             if password == confirm:
                 try:
                     user = User.objects.get(username=username)
@@ -70,10 +70,10 @@ def register_Company(request):
         phone = request.POST['Phone']
         # api call
         response = requests.get(
-             #'https://jsonplaceholder.typicode.com/todos/1')
-            f'https://app.verify-email.org/api/v1/UBTW2fIw7W2yR5ICPldZGuah72wKN8gTsAB7aho06HoiKw9ySC/verify/{email}')
+             'https://jsonplaceholder.typicode.com/todos/1')
+            #f'https://app.verify-email.org/api/v1/UBTW2fIw7W2yR5ICPldZGuah72wKN8gTsAB7aho06HoiKw9ySC/verify/{email}')
         data = response.json();
-        if (data['status'] == 1):  # id=1
+        if (data['id'] == 1):  # status=1
             if password == confirm:
                 try:
                     user = User.objects.get(username=cname)
