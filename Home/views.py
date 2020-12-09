aafa043d0e8b3fd9de90bc6bbbd9801c41802d87
amatyaichhya <amatyaichhya@gmail.com>
2020-12-09 15:24:56 +0545

    rebase 7/11 conflicts resolved

--- Home/views.py
+++ Home/views.py
@@ -173,4 +173,3 @@ def search(request):
     job_list = JobRequirements.objects.filter(post__icontains=search)
     return render(request, 'Home/search_result.html', {'jobs': job_list, 'search': search})
 
-
